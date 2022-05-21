621ac375f955a832d0ffd11cc5b75029faf98563
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-22 00:17:01 +0600

    basic route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+import About from './pages/About/About';
+import Login from './pages/Account/Login';
+import Signup from './pages/Account/Signup';
+import Blog from './pages/Blog/Blog';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import { Toaster } from 'react-hot-toast';
+import NotFound from './components/NotFound';
 
 const App = () => {
     return (
         <div>
-            <button class="btn btn-primary">Button</button>
+            <Navbar>
+                <ScrollToTop>
+                    <Routes >
+                        <Route path='/' element={<Home />}></Route>
+                        <Route path='/blog' element={<Blog />}></Route>
+                        <Route path='/about' element={<About />}></Route>
+                        <Route path='/contact' element={<Contact />}></Route>
+                        <Route path='/login' element={<Login />}></Route>
+                        <Route path='/signup' element={<Signup />}></Route>
+                        <Route path='*' element={<NotFound />}></Route>
+                    </Routes>
+                    <Toaster />
+                </ScrollToTop>
+            </Navbar>
         </div>
     );
 };

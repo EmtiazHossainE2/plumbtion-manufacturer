ee4c1bd538c3b06b4ef51e2588c2705d22b4e906
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-22 01:09:14 +0600

    images and icons added

---
+++ __notes.js
@@ -0,0 +1,27 @@
+/**
+ * images 
+https://i.ibb.co/9GBLfHR/logo1.png
+https://i.ibb.co/0rjjVs2/banner1.jpg
+https://i.ibb.co/6ZZyQH7/banner2.jpg
+https://i.ibb.co/PxZqHgv/banner3.jpg
+https://i.ibb.co/gtJM2Hx/img1.jpg
+https://i.ibb.co/dt5cDJq/img2.jpg
+https://i.ibb.co/qpY1mfH/img4.jpg
+https://i.ibb.co/jLJsWSp/pipe1.jpg
+https://i.ibb.co/Z62tB9N/pipe2.jpg
+https://i.ibb.co/tBsmnqN/pipe3.jpg
+https://i.ibb.co/sHpTDSK/man1.jpg
+https://i.ibb.co/7CPfwBB/man2.jpg
+https://i.ibb.co/mJL2zQV/man3.jpg
+https://i.ibb.co/fn25XQz/man4.jpg
+https://i.ibb.co/WncZWQg/man5.jpg
+https://i.ibb.co/gtz7TBm/man6.jpg
+
+
+icons 
+https://i.ibb.co/xCKnYpK/1.png
+https://i.ibb.co/KNwgyn2/2.png
+https://i.ibb.co/0y2W5Bb/3.png
+https://i.ibb.co/Dr6Hddw/4.png
+https://i.ibb.co/M5hdN6F/5.png
+ */

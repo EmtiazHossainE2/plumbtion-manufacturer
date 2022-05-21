58677a31b1359879ecf311a4edba663adb90ccd5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-22 00:10:13 +0600

    important pages and component create

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>This page is not available </h2>
+            <p>404</p>
+        </div>
+    );
+};
+
+export default NotFound;

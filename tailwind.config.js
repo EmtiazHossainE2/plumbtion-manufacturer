085d78f9e56c61e93b07948fb987292e58d5863d
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-21 23:57:30 +0600

    theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        plumbtion: {
+          "primary": "#FF5316",
+          "secondary": "#F3A71C",
+          "accent": "#07499B",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#C41919",
+        },
+      },
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
 }

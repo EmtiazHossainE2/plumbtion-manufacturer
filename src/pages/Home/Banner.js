9701ab8408b82dc5004ad27d654eb58671af2954
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-22 01:29:56 +0600

    banner section done

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <h2>This is banner section</h2>
+            <div className="py-12">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div >
+                        <img src="https://i.ibb.co/fn25XQz/man4.jpg" className="max-w-sm lg:max-w-lg rounded-lg shadow-2xl" alt='man' />
+                    </div>
+                    <div className='lg:px-8 font-bold space-y-2'>
+                        <h1 className="text-4xl ">Plumbtion is well established </h1>
+                        <h1 className="text-3xl ">company manufacturing </h1>
+                        <h1 className="text-3xl ">Tubes & Pipes </h1>
+                        <p className="py-6 text-lg ">We also manufacture special steel pipes & tubes
+                            like Inconel, duplex, super duplex etc.</p>
+                        <button className="btn btn-primary px-8">read more </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home page</h2>
+            <div className="lg:px-12">
+                <Banner />
+            </div>
         </div>
     );
 };

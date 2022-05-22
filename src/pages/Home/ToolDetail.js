45376f633ebb62ff6e19473006f13674a5d2039d
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-23 04:53:34 +0600

    calculation for order setion

--- src/pages/Home/ToolDetail.js
+++ src/pages/Home/ToolDetail.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Footer from '../../components/Footer';
 import Loading from '../../components/Loading';
 import auth from '../../Firebase/firebase.init';
 
@@ -11,22 +13,69 @@ const ToolDetail = () => {
 
     const { data: detail, isLoading, refetch } = useQuery('detail', () => fetch(`http://localhost:5000/tool/${toolId}`).then(res => res.json()))
 
-    // console.log(detail);
+
 
     if (isLoading) {
         return <Loading />
     }
 
+
     const handlePurchase = event => {
         event.preventDefault()
-        const purchaseInfo = {
-            toolName: detail?.name,
-            due: detail?.price,
-            userName: user?.displayName,
-            email: user?.email,
-            address: event.target.address.value,
-            phone: event.target.phone.value
+
+        const orderQuantity = parseInt(event.target.quantity.value)
+        const totalPrice = parseInt(orderQuantity) * parseInt(detail?.price)
+
+        if (orderQuantity < parseInt(detail?.minOrder)) {
+            Swal.fire({
+                text: `Min Order is ${detail?.minOrder} pice`,
+                icon: 'error',
+                confirmButtonText: 'Okay'
+            })
+            return
         }
+        if (orderQuantity > parseInt(detail?.available)) {
+            Swal.fire({
+                text: `We don't have sufficient tool . Max order is ${detail?.available}`,
+                icon: 'error',
+                confirmButtonText: 'Okay'
+            })
+            return
+        }
+        else{
+            const purchaseInfo = {
+                userName: user?.displayName,
+                email: user?.email,
+                pipeName: detail?.name,
+                totalPrice,
+                orderQuantity,
+                address: event.target.address.value,
+                phone: event.target.phone.value
+            }
+    
+            
+            if (purchaseInfo.address === '') {
+                Swal.fire({
+                    text: "Provide Original Address",
+                    icon: 'error',
+                    confirmButtonText: 'Okay'
+                })
+                return
+            }
+            if (purchaseInfo.phone === '') {
+                Swal.fire({
+                    text: 'Provide Phone Number',
+                    icon: 'error',
+                    confirmButtonText: 'Okay'
+                })
+                return
+            }
+            else{
+                console.log(purchaseInfo);
+            }
+        }
+
+        
 
     }
 
@@ -41,7 +90,7 @@ const ToolDetail = () => {
                     </div>
                     <div className="flex-1 px-5">
                         <div className='space-y-4'>
-                            <h2 className='text-2xl pt-5 lg:mt-0 capitalize'>Tool Name : {detail?.name}</h2>
+                            <h2 className='text-2xl pt-5 lg:mt-0 capitalize'>Pipe Name : {detail?.name}</h2>
                             <h2 className='text-xl font-bold pt-5'>Features :</h2>
                             <div className=''>
                                 <p>Length  : {detail?.length}</p>
@@ -59,19 +108,19 @@ const ToolDetail = () => {
                 </div>
                 <hr className='bg-red-500 my-5 lg:my-16' />
                 <div className='pb-8  '>
-                    <div className="flex flex-col lg:flex-row justify-center ">
+                    <div className="flex flex-col lg:flex-row justify-center space-y-12 lg:space-y-0">
                         <div className='text-center flex-1'>
                             <h3 className="font-bold text-3xl "> Place Order Here </h3>
                             <form onSubmit={handlePurchase} className='space-y-4 pt-8 '>
                                 <input type="text" disabled value={user?.displayName || ''} name='userName' className="input input-bordered w-full max-w-md text-lg" />
                                 <input type="email" disabled value={user?.email || ''} name='email' className="input input-bordered w-full max-w-md text-lg" />
-                                <input type="number" name='quantity' placeholder='Quantity' className="input input-bordered w-full max-w-md text-lg" />
+                                <input type="number" name='quantity' placeholder='Quantity' className="input input-bordered w-full max-w-md text-lg" required/>
                                 <textarea rows={2} type="text" placeholder='Your Address' name='address' className=" input-bordered w-full textarea max-w-md text-lg " />
                                 <input type="number" placeholder="Phone Number" name='phone' className="input input-bordered w-full max-w-md text-lg" />
                                 <input type="submit" value="Submit" className="btn btn-secondary text-white w-full max-w-md text-lg" />
                             </form>
                         </div>
-                        <div className='flex-1 hidden lg:block'>
+                        <div className='flex-1 '>
                             <div>
                                 <h2 className='text-center text-3xl'>Our Sponsors</h2>
                                 <div className='space-y-2'>
@@ -96,6 +145,7 @@ const ToolDetail = () => {
                     </div>
                 </div>
             </div>
+            <Footer />
         </div>
     );
 };

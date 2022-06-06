f124cf613480c5b80afd70d71ae371a09325b560
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-06-07 02:13:59 +0600

    display all review

---
+++ src/pages/Dashboard/Review/ManageReviewRow.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+import demoProfile from '../../../assets/images/demoProfile.png'
+
+const ManageReviewRow = ({ review, index, refetch }) => {
+    const { _id , name, country ,rating } = review
+    console.log(review);
+
+    const handleDelete = () => {
+        const url = `https://plumbtion-manufacturer.herokuapp.com/review/${_id}`;
+        Swal.fire({
+            text: `Are you sure to delete  ?`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Delete",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(url, {
+                    method: 'DELETE',
+                    headers: {
+                        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                    },
+                }).then(data => {
+                    // console.log(data);
+                    if (data.status) {
+                        Swal.fire({
+                            text: `Successfully Delete `,
+                            icon: 'success',
+                            confirmButtonText: 'Okay'
+                        })
+                        refetch()
+                    }
+                })
+            }
+        })
+
+    }
+
+
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>
+                {review?.img
+                    ? (
+                        <div className="avatar">
+                            <div className="w-12 rounded-full">
+                                <img src={review?.img} alt="User Profile" />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="avatar">
+                            <div className="w-12 rounded">
+                                <img src={demoProfile} alt="Demo Profile" />
+                            </div>
+                        </div>
+                    )
+                }
+            </td>
+            <td>{name}</td>
+            <td>{country}</td>
+            <td>{rating}</td>
+            <td><button onClick={handleDelete} className='btn btn-xs btn-error'>Delete</button></td>
+        </tr>
+    );
+};
+
+export default ManageReviewRow;
--- src/pages/Dashboard/Review/ManageReviews.js
+++ src/pages/Dashboard/Review/ManageReviews.js
@@ -1,9 +1,72 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Loading from '../../../components/Loading';
+import PageTitle from '../../../components/PageTitle';
+import auth from '../../../Firebase/firebase.init';
+import ManageReviewRow from './ManageReviewRow';
 
 const ManageReviews = () => {
+    const navigate = useNavigate()
+
+    const { data: reviews, isLoading, refetch } = useQuery('reviews', () => fetch(`https://plumbtion-manufacturer.herokuapp.com/review`, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        },
+    })
+        .then(res => {
+            if (res.status === 401 || res.status === 403) {
+                signOut(auth)
+                localStorage.removeItem('accessToken')
+                Swal.fire({
+                    text: 'Session expired sign in again . .',
+                    icon: 'error',
+                    confirmButtonText: 'Okay'
+                })
+                navigate('/login')
+            }
+            return res.json()
+        }))
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    console.log(reviews);
+
     return (
         <div>
-            Manage Reviews
+            <PageTitle title="Manage Reviews -"></PageTitle>
+            <h2 className='md:p-4 text-xl'>Manage Reviews {reviews.length} </h2>
+            <div className='md:p-4'>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Country </th>
+                                <th>Rating </th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                reviews.map((review, index) => <ManageReviewRow
+                                    key={index}
+                                    index={index}
+                                    review={review}
+                                    refetch={refetch}
+                                ></ManageReviewRow>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
